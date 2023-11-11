47bf12fe0c7eb135927b030fcd7e44425d12f345
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-11-11 16:18:45 +0100

    trying out path

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,5 @@
-import { useCountryContext } from "@hooks";
 import IonIcon from "@components/Ion";
+import { useCountryContext } from "@hooks";
 
 const Search = () => {
     const {search} = useCountryContext();
