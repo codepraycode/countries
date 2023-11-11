e501cd5ff05593ce82c037cef1941b4f282c0468
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-11-11 16:01:40 +0100

    Aded the icon component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-
+import IonIcon from "./Ion";
 
 const Header = () => {
     return (
@@ -9,7 +9,7 @@ const Header = () => {
                 <h1 className="fw-800 fs-md-17 fs-sm-17">Where in the world?</h1>
 
                 <button className="d-flex align-center btn border-none bg-transparent">
-                    <ion-icon name="moon-outline"></ion-icon>
+                    <IonIcon name="moon-outline"/>
 
                     <span className="ml-07 fs-16 fw-600">Dark Mode</span>
                 </button>
---
+++ src/components/Ion.tsx
@@ -0,0 +1,2 @@
+const IonIcon = ({name}: {name: string}) => <ion-icon name={name}></ion-icon>
+export default IonIcon;
